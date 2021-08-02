c17864371d37d692e9e8821fff4b3f97d3d3046e
David Júnior <david.junior@riversoft.com.br>
2021-08-02 16:44:41 -0300

    MOD.: apply Layout and first implementations in form survey.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,21 @@ import Link from 'next/link'
 const Index = () => {
 	return (
     <div>
-      <h1>Olá Semana Fullstack Master!</h1>
+      <p className='mt-12 text-center'>
+        O restaurante X sempre busca por atender melhor seus clientes.
+        <br />
+        Por isso, estamos sempre abertos a ouvir a sua opnião.
+      </p>
+
+      <div className='text-center my-12'>
+        <Link href='/pesquisa'>
+          <a className='bg-blue-400 px-6 py-4 font-bold rounded-lg hover:shadow-lg'>
+            Dar opinião ou sugestão
+          </a>
+        </Link>
+      </div>
+
+      <p className='my-12 text-center'>Mensagem do desconto.</p>
     </div>
   );
 }
